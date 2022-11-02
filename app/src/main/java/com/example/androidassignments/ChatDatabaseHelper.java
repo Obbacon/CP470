be7696a2f0af328121e305a07dd48635fe01e735
Obbacon <rayx70@live.ca>
2022-11-02 19:34:49 -0400

    Finished Assignment 3, part 1

---
+++ app/src/main/java/com/example/androidassignments/ChatDatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.example.androidassignments;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import androidx.annotation.Nullable;
+
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+    public static final String KEY_MESSAGE = "message";
+    public static final String TABLE_NAME = "messagesTable";
+    public static final String KEY_ID = "Key_ID";
+    public static final int VERSION_NUM = 3;
+    public static final String DATABASE_NAME = "Messages.db";
+
+    public static final String CREATE_DATABASE = "create table " + TABLE_NAME + "(" + KEY_ID + " integer primary key autoincrement, " + KEY_MESSAGE + " text not null);";
+
+    public ChatDatabaseHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_DATABASE);
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVersion + " newVersion=" + newVersion);
+    }
+}
--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -2,10 +2,16 @@ package com.example.androidassignments;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
@@ -21,6 +27,10 @@ public class ChatWindow extends AppCompatActivity {
     ListView chats;
     ArrayList<String> msg;
     ChatAdapter messageAdapter;
+    private SQLiteDatabase Database;
+    private ChatDatabaseHelper databaseHelper;
+
+    @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +40,41 @@ public class ChatWindow extends AppCompatActivity {
         newMessage = findViewById(R.id.editMessage);
         chats = findViewById(R.id.chatView);
 
+        databaseHelper = new ChatDatabaseHelper(this);
+        Database = databaseHelper.getWritableDatabase();
+        String[] getColumns = {ChatDatabaseHelper.KEY_MESSAGE};
+        Cursor newCursor = Database.query(ChatDatabaseHelper.TABLE_NAME, getColumns, null, null, null, null, null);
+        newCursor.moveToFirst();
+
+        while(!newCursor.isAfterLast()){
+            msg.add(newCursor.getString(newCursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+            Log.i("ChatWindow.java","SQL MESSAGE: " + newCursor.getString(newCursor.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+            newCursor.moveToNext();
+        }
+        Log.i("ChatWindow.java", "Cursor's column count =" + newCursor.getColumnCount());
+
+        for(int i = 0; i < newCursor.getColumnCount(); i++){
+            Log.i("ChatWindow.java", newCursor.getColumnName(i));
+        }
+        newCursor.close();
+
         messageAdapter = new ChatAdapter(this);
         chats.setAdapter(messageAdapter);
 
         buttonSend.setOnClickListener(v -> {
             msg.add(newMessage.getText().toString());
+            ContentValues newValue = new ContentValues();
+            newValue.put(ChatDatabaseHelper.KEY_MESSAGE, newMessage.getText().toString());
+            Database.insert(ChatDatabaseHelper.TABLE_NAME, null, newValue);
             messageAdapter.notifyDataSetChanged();
             newMessage.setText("");
         });
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Database.close();
     }
 
     private class ChatAdapter extends ArrayAdapter<String>{
